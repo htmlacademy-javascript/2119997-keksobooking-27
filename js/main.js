7504e77f4203c1bb4a82b8cdb124e9ba5fb332c6
MetlikGG <kurochkinalexy@mail.ru>
2022-10-05 23:39:06 +0300

    Правки к заданию

--- js/main.js
+++ js/main.js
@@ -1,21 +1,9 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 
-function getRandomInt(min,max) {
-  if (min < max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {
-    return 'Некорректный диапазон';
-  }
-}
+const getRandomInt = (min, max) => min < max ? Math.floor(Math.random() * (max - min + 1)) + min : 'Некорректный диапазон';
 getRandomInt(0,10);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
-function getRandomNonInt(min,max,symbols) {
-  if (min < max) {
-    return +(Math.random() * (max - min + min)).toFixed(symbols);
-  } else {
-    return 'Некорректный диапазон';
-  }
-}
+const getRandomNonInt = (min, max, symbols) => min < max ? +(Math.random() * (max - min + min)).toFixed(symbols) : 'Некорректный диапазон';
 getRandomNonInt(0.1,10,3);
